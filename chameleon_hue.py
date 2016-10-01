65bbc0ba5d7a9a2b42f9439b271802268878eec9
VSZM _ <VSZM _>
2016-10-01 03:15:40 +0200

    Finished with implementation of backend

--- chameleon_hue.py
+++ chameleon_hue.py
@@ -16,19 +16,19 @@ class HueChameleon:
     def lampStateChange(self):        
         while True:
             state = self.lampStateQueue.get()
-            lamp_before = time.time()
+            #lamp_before = time.time()
             self.lamp.set_color_rgb(state[0], state[1], state[2], state[3])
-            lamp_after = time.time()
-            print('\tChanging lamp state took {} ms'.format((lamp_after - lamp_before) * 1000.0))
+            #lamp_after = time.time()
+            #print('\tChanging lamp state took {} ms'.format((lamp_after - lamp_before) * 1000.0))
 
         
     def tick(self):
         global lampChangerThread
         #taking screenshot
-        screenshot_before = time.time()
+        #screenshot_before = time.time()
         screenshot = ImageGrab.grab()
-        screenshot_after = time.time()
-        print('\tTaking screenshot took {} ms'.format((screenshot_after - screenshot_before) * 1000.0))
+        #screenshot_after = time.time()
+        #print('\tTaking screenshot took {} ms'.format((screenshot_after - screenshot_before) * 1000.0))
 
         # Getting avg color of screen
         R = 0
@@ -48,10 +48,10 @@ class HueChameleon:
         BrightnessPercentage = (R + G + B) / (255.0*3)
         
         # Wait for lampthread to finish
-        wait_before = time.time()
+        #wait_before = time.time()
         self.lampStateQueue.put((R,G,B,BrightnessPercentage))
-        wait_after = time.time()
-        print('\tHad to wait for lampthread for {} ms'.format((wait_after - wait_before) * 1000.0))
+        #wait_after = time.time()
+        #print('\tHad to wait for lampthread for {} ms'.format((wait_after - wait_before) * 1000.0))
         
         
         
--- hue_controls.py
+++ hue_controls.py
@@ -1,12 +1,12 @@
 from rgb_cie import Converter
 import qhue
-from tornado.httpclient import HTTPClient, HTTPRequest
-import tornado.web
 import requests
+import math
 
 
 username = 'ejLH4WtpkUuagMV4epRQRxA04uyBnQaesVnxAMxF'
 ip = '192.168.0.100'
+change_treshold = 0.05
 
 def get_lamp_id_by_name(bridge, lampname):
     lights = bridge.lights()
@@ -16,12 +16,7 @@ def get_lamp_id_by_name(bridge, lampname):
     
     return None
 
-
-def handleResponse(self, response):
-        print("callback")
-        print(response.body)
-
-class Lamp(tornado.web.RequestHandler):
+class Lamp():
     
     converter = Converter()
     
@@ -30,27 +25,37 @@ class Lamp(tornado.web.RequestHandler):
         self.bridge = qhue.Bridge(ip, username)
         self.lampid = get_lamp_id_by_name(self.bridge, lampname)
         self.url = self.bridge.lights[self.lampid].url + '/state'
-        self.http_client = HTTPClient()
-        self.on = 
-
+        self.on = self.bridge.lights[self.lampid]()['state']['on']
+        self.xy = self.bridge.lights[self.lampid]()['state']['xy']
+        self.brightnessPercent = self.bridge.lights[self.lampid]()['state']['bri'] / 255.0
 
     def set_color_rgb(self, r, g, b, brightnessPercent = None):
         xy = self.converter.rgbToCIE1931(r, g, b)
         self.set_color_xy(xy, brightnessPercent)
         
         
-    @tornado.web.asynchronous
     def set_color_xy(self, xyval, brightnessPercent = None):
-        if brightnessPercent == None:
-            self.on = True
-            requests.put(self.url, data='{ "xy": [' + xyval[0] + ', ' + xyval[1] + '], "on": true, "transitiontime" = 0}')
-            #self.bridge.lights[self.lampid].state(xy = xyval, on = True, transitiontime = 0)
-        elif brightnessPercent < 0.05:
-            requests.put(self.url, data='{ "on": false, "transitiontime" = 0}')
-            #self.bridge.lights[self.lampid].state(on = False, transitiontime = 0)
-        else:
-            self.http_client.fetch(HTTPRequest(url=self.url, method="PUT", body='{ "xy": [' + str(xyval[0]) + ', ' + str(xyval[1]) + '], "bri": ' + str(int(brightnessPercent * 255)) + ', "on": true, "transitiontime": 0}'))
-            #requests.put(self.url, data='{ "xy": [' + str(xyval[0]) + ', ' + str(xyval[1]) + '], "bri": ' + str(int(brightnessPercent * 255)) + ', "on": true, "transitiontime": 0}')
-            #self.bridge.lights[self.lampid].state(xy = xyval, bri = int(brightnessPercent * 255), on = True, transitiontime = 0)
+        xdiff = math.fabs(xyval[0] - self.xy[0])
+        ydiff = math.fabs(xyval[1] - self.xy[1])
+        color_diffpercent = (xdiff + ydiff) / 1.0
         
-        tornado.ioloop.IOLoop.instance().start()
+        if brightnessPercent is None:
+            brightnessPercent = self.brightnessPercent        
+        brightness_diffpercent = math.fabs(self.brightnessPercent - brightnessPercent)
+        
+        if color_diffpercent < change_treshold and brightness_diffpercent < change_treshold:
+            return
+        
+        
+        if brightnessPercent < 0.05:
+            requests.put(self.url, data='{ "on": false, "transitiontime" : 0}')
+            self.on = False
+        else:
+            if self.on == True:
+                requests.put(url=self.url, data='{ "xy": [' + str(xyval[0]) + ', ' + str(xyval[1]) + '], "bri": ' + str(int(brightnessPercent * 255)) + ', "transitiontime": 1}')
+            else:
+                requests.put(self.url, data='{ "xy": [' + str(xyval[0]) + ', ' + str(xyval[1]) + '], "bri": ' + str(int(brightnessPercent * 255)) + ', "on": true, "transitiontime": 1}')
+                self.on = True
+        
+        self.xy = xyval
+        
